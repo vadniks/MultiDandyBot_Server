3d7c29fa197a00f27f61866884a982b4426c60d2
vadniks <vadniks@fk.fk>
2022-04-25 13:16:10 +0300

    Continuing

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3 as sq
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Any
 
 from sync import Player
 import atexit as ax
@@ -18,11 +18,13 @@ _connection = sq.connect(_DB_FILE)
 ax.register(lambda: _connection.close()) #TODO: make client's quit request sending in this way
 
 
-def _wrapper(wrapped: Callable):
+def _wrapper(wrapped: Callable, doPost: Callable = None) -> Any:
     cursor = _connection.cursor()
-    wrapped(cursor)
+    a = wrapped(cursor)
     _connection.commit()
     cursor.close()
+    if doPost is not None: doPost(a)
+    return a
 
 
 # noinspection SqlNoDataSourceInspection
@@ -45,3 +47,8 @@ def insert(player: Player): _wrapper(lambda cursor:
         {player.script},
         {player.created}
     )'''))
+
+
+# noinspection SqlNoDataSourceInspection
+def select(): _wrapper(lambda cursor:
+    cursor.execute(f'''select * from {_DB_NAME} order by {_DB_SCORE} desc'''))
