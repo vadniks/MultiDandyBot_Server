60f47fd42c5d17133af2d5908e403f1960a0de92
vadniks <vadniks@fk.fk>
2022-04-27 13:24:34 +0300

    Continuing

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>There's no easter eggs up here, go away.</h1>"
+    return "<h1>There are no easter eggs up here, go away.</h1>"
 
 
 @app.route('/new', methods=['POST'])
